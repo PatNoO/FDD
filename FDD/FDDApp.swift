c6e3380b6de8c56592e71dca7a8d4c11b6ad5cb3
PatNoO <Noordh94@hotmail.se>
2026-03-25 13:20:41 +0100

    Initial Commit

---
+++ FDD/FDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDDApp.swift
+//  FDD
+//
+//  Created by Patrik Noordh on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct FDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
